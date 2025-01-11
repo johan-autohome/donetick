c75679c9a6858a0786049f476be4596962ab5503
johan-autohome <johan.autohome@gmail.com>
2025-01-12 00:47:37 +0100

    add complete endpoint to external api

--- internal/chore/api.go
+++ internal/chore/api.go
@@ -1,6 +1,10 @@
 package chore
 
 import (
+	"net/http"
+	"strconv"
+	"time"
+
 	"donetick.com/core/config"
 	chRepo "donetick.com/core/internal/chore/repo"
 	"donetick.com/core/internal/utils"
@@ -44,6 +48,58 @@ func (h *API) GetAllChores(c *gin.Context) {
 	c.JSON(200, chores)
 }
 
+func (h *API) PostCompleteChore(c *gin.Context) {
+	apiToken := c.GetHeader("secretkey")
+	if apiToken == "" {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+		return
+	}
+	user, err := h.userRepo.GetUserByToken(c, apiToken)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+		return
+	}
+
+	id := c.Param("id")
+	choreId, err := strconv.Atoi(id)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Bad id"})
+		return
+	}
+
+	chore, err := h.choreRepo.GetChore(c, choreId)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Chore NotFound"})
+		return
+	}
+
+	choreHistory, err := h.choreRepo.GetChoreHistory(c, choreId)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error getting history"})
+		return
+	}
+
+	nextAssignedTo, err := checkNextAssignee(chore, choreHistory, user.ID)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error checking next assignee"})
+		return
+	}
+
+	completedAt := time.Now()
+	err = h.choreRepo.CompleteChore(c, chore, nil, user.ID, chore.NextDueDate, &completedAt, nextAssignedTo)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error completing chore"})
+		return
+	}
+
+	chore, err = h.choreRepo.GetChore(c, choreId)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error getting chore"})
+		return
+	}
+	c.JSON(200, chore)
+}
+
 func APIs(cfg *config.Config, api *API, r *gin.Engine, auth *jwt.GinJWTMiddleware, limiter *limiter.Limiter) {
 
 	thingsAPI := r.Group("eapi/v1/chore")
@@ -51,6 +107,7 @@ func APIs(cfg *config.Config, api *API, r *gin.Engine, auth *jwt.GinJWTMiddlewar
 	thingsAPI.Use(utils.TimeoutMiddleware(cfg.Server.WriteTimeout), utils.RateLimitMiddleware(limiter))
 	{
 		thingsAPI.GET("", api.GetAllChores)
+		thingsAPI.POST("/:id", api.PostCompleteChore)
 	}
 
 }
